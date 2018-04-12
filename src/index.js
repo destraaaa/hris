fd11e10a9fd356bc05a52e919f55db84d81881b0
aryl000 <aryl.otniel@tokopedia.com>
2018-04-12 11:26:10 +0700

    Put style edit in DashBoard

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import App from 'containers/App/App.jsx';
 import './assets/css/bootstrap.min.css';
 import './assets/css/animate.min.css';
 import './assets/sass/light-bootstrap-dashboard.css';
+import './assets/sass/dashboardEdit.css';
 import './assets/css/demo.css';
 import './assets/css/pe-icon-7-stroke.css';
 import './assets/css/index.css';
