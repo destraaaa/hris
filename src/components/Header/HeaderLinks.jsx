5d605676eddca2745d5536b05989c7e8af0970f4
aryl000 <aryl.otniel@tokopedia.com>
2018-04-20 17:42:11 +0700

    google sign in

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -1,24 +1,136 @@
+/* global gapi */
 import React, {Component} from 'react';
 import { NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap';
-
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-};
+import GoogleSignIn from "react-google-signin";
+import {PostData} from"./PostData";   
 
 class HeaderLinks extends Component{
+    constructor(){
+        super();
+        this.onSignIn = this.onSignIn.bind(this)
+    }
+    componentDidMount () {
+        const script = document.createElement("script");
+        console.log("this script")
+        script.src = "https://apis.google.com/js/platform.js?onload=onLoadCallback";
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+
+        window.onLoadCallback = function(){
+            console.log("this window callback")
+                
+                gapi.auth2.init({
+                    client_id: '667718253087-qeek1sjq841eo473bi8sqq64evqo16ss.apps.googleusercontent.com'
+                 
+                });
+              }
+    }
     
+
+    // init() {
+    //     gapi.load('auth2', function() { 
+    //         client_id:'667718253087-qeek1sjq841eo473bi8sqq64evqo16ss.apps.googleusercontent.com'
+    //         // scope: 'email'
+    //   })
+    // }
+    // signOut() {
+    //     var auth2 = gapi.auth2.getAuthInstance();
+    //     auth2.signOut().then(function () {
+    //       console.log('User signed out.');
+    //     });
+    //   }
+
+    // componentDidMount() {
+    //     gapi.signin2.render('g-signin2', {
+    //       'scope': 'https://www.googleapis.com/auth/plus.login',
+    //       'width': 200,
+    //       'height': 50,
+    //       'longtitle': true,
+    //       'theme': 'dark',
+    //       'onsuccess': this. onSignIn
+    //     });  
+        
+    //   }
+    onSignIn(googleUser) {
+        console.log("this onsignin")
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+    
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+    };
+        // window.onLoadCallback = function(){
+        // gapi.auth2.init({
+        //     client_id: '667718253087-qeek1sjq841eo473bi8sqq64evqo16ss.apps.googleusercontent.com'
+        //   });
+//     constructor(props) {
+//         super(props);
+//            this.state = {
+//         //    loginError: false,
+//         redirectToReferrer: false
+//     };
+//     this.signup = this.signup.bind(this);
+//     }
+
+//     signup (res) {
+//         let postData;
+
+//         PostData('signup',postData).then((res)=>
+//     {
+//         let responseJson = res;
+//         if(responseJson.userData){
+//             sessionStorage.setItem('userData',JSON.stringify(responseJson));
+//             this.setState({redirectToReferrer:true});
+//         }
+//     }
+//     )
+//         // postData = {
+//         //     name: res.w3.ig,
+//         //     provider: type,
+//         //     email: res.w3.U3,
+//         //     provider_id: res.El,
+//         //     token: res.Zi.access_token,
+//         //     provider_pic: res.w3.Paa
+//         //   };
+
+//         if (postData) {
+//         PostData('signup', postData).then((result) => {
+//             let responseJson = result;
+//             sessionStorage.setItem("userData", JSON.stringify(responseJson));
+//             this.setState({redirect: true});
+//         });
+//     }
+// }
+//     onSignIn(userProfile, accessToken) {
+//         console.log(userProfile)
+//     }
+    
+    signOut() {
+        // this.googleAuth.signOut();
+        console.log("asdasd")
+    }
+
+//     responseGoogle (googleUser) {
+//         var id_token = googleUser.getAuthResponse().id_token;
+//         console.log({accessToken: id_token});
+//         //anything else you want to do(save to localStorage)...
+//       }
     render(){
+
+        // const responseGoogle = (response) => {
+        //     console.log("google console");
+        //     console.log(response);
+        //     this.signup(response);
+        // }
+
         const notification = (
             <div>
                 <i className="fa fa-globe"></i>
@@ -50,12 +162,27 @@ class HeaderLinks extends Component{
                     {/* <NavItem eventKey={1} href="http://0.0.0.0:8080/authLogin">Login</NavItem> */}
                     <NavItem eventKey={1}> <div 
                             className="g-signin2"
-                            data-onsuccess="onSignIn"
+                            data-onsuccess={this.onSignIn}
                             data-theme="dark"
+                            buttonText="blbalbal"
                             style={{position:"relative", bottom:8}}
                             >
-                            sign 
+                            
                             </div>
+                    </NavItem>
+                    <NavItem eventKey={1}>
+                    <div style={{position:"relative", bottom:5, width:170}}>
+                     {/* <GoogleSignIn 
+                          clientId="667718253087-qeek1sjq841eo473bi8sqq64evqo16ss.apps.googleusercontent.com"
+                          width ="180"
+                          height = "30"
+                          buttonText = "Login"
+                          onSuccess={responseGoogle}
+                          onFailure={responseGoogle}
+                        //   ref={g => this.googleAuth = g}
+                        //   onSuccess={this.onSignIn.bind(this)}
+                     /> */}
+                     </div>
                     </NavItem>
                     <NavDropdown eventKey={2} title="Dropdown" id="basic-nav-dropdown-right">
                         <MenuItem eventKey={2.1}>Action</MenuItem>
